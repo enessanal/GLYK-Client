47b0dd96d9616b04e997b4843e94c50760a94e2f
ES <enessanal@protonmail.com>
2023-08-21 22:58:24 +0300

    Seperated navbar to another component

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,6 @@
 <template>
   <div class="container-fluid container-md">
-    <nav class="navbar navbar-expand-sm navbar-light bg-light prevent-select">
-      <div class="container-fluid d-flex justify-content-center">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse my-1 text-center" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <b><router-link class="nav-link" :to="{ name: 'HomeView' }">Home</router-link></b>
-            </li>
-            <li class="nav-item">
-              <b><router-link class="nav-link" :to="{ name: 'CustomerListView' }">Customers</router-link></b>
-            </li>
-            <li class="nav-item">
-              <b><router-link class="nav-link" :to="{ name: 'ProductListView' }">Products</router-link></b>
-            </li>
-            <li class="nav-item">
-              <b><router-link class="nav-link" :to="{ name: 'UserListView' }">Users</router-link></b>
-            </li>
-            <li class="nav-item">
-              <b><router-link class="nav-link" :to="{ name: 'CartView' }">Cart</router-link></b>
-            </li>
-            <li class="nav-item">
-              <b><router-link class="nav-link" :to="{ name: 'InvoiceListView' }">Invoices</router-link></b>
-            </li>
-          </ul>
-
-        </div>
-      </div>
-    </nav>
+    <Navbar/>
     <hr>
     <div class="bg-light py-3">
       <router-view />
@@ -41,6 +12,18 @@
   </div>
 </template>
 
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default
+{
+  components:
+  {
+    Navbar
+  }
+}
+</script>
 
 
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light prevent-select">
+      <div class="container-fluid d-flex justify-content-center">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse my-1 text-center" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <b><router-link class="nav-link" :to="{ name: 'HomeView' }">Home</router-link></b>
+            </li>
+            <li class="nav-item">
+              <b><router-link class="nav-link" :to="{ name: 'CustomerListView' }">Customers</router-link></b>
+            </li>
+            <li class="nav-item">
+              <b><router-link class="nav-link" :to="{ name: 'ProductListView' }">Products</router-link></b>
+            </li>
+            <li class="nav-item">
+              <b><router-link class="nav-link" :to="{ name: 'UserListView' }">Users</router-link></b>
+            </li>
+            <li class="nav-item">
+              <b><router-link class="nav-link" :to="{ name: 'CartView' }">Cart</router-link></b>
+            </li>
+            <li class="nav-item">
+              <b><router-link class="nav-link" :to="{ name: 'InvoiceListView' }">Invoices</router-link></b>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+    </nav>
+</template>
