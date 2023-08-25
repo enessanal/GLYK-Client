adb7cdacae9ffafe1d40e8190054ac0abb0c8a54
ES <enessanal@protonmail.com>
2023-08-25 14:23:21 +0300

    Uninstall npm unneccesary packages (json-server, concurrently)

--- vue.config.js
+++ vue.config.js
@@ -7,20 +7,6 @@ module.exports =
   devServer:
   {
     host: "localhost",
-    port: 8081,
-    proxy:
-    {
-        '^/api':
-        {
-            target: 'http://localhost:5000',
-            changeOrigin: true,
-            logLevel: 'debug',
-            pathRewrite:
-            {
-              '^/api':'/'
-            }
-        }
-    }
+    port: 8081
   }
-
 }
