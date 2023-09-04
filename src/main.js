3908e872cdc659c2a630c58fa3c90591850b4cc0
ES <enessanal@protonmail.com>
2023-09-04 18:56:57 +0300

    Installed "vue-i18n" dependency for multi-language.

--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import axios from "axios";
+import { createI18n } from 'vue-i18n'
+
+const i18n = createI18n({
+    // something vue-i18n options here ...
+  })
+
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 axios.defaults.baseURL = "http://localhost:8080/api/v1/";
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+.use(router)
+.use(i18n)
+.mount('#app')
